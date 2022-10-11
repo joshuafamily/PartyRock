13b1dd66cfa40f5553ab4540a52d9d2d71a25a3a
xiaomagua <1170556710@qq.com>
2022-10-11 21:13:37 +0800

    add files

--- PartyRockApp/PartyCell.swift
+++ PartyRockApp/PartyCell.swift
@@ -10,15 +10,31 @@ import UIKit
 
 class PartyCell: UITableViewCell {
 
+    @IBOutlet weak var videoPreviewImage: UIImageView!
+    
+    @IBOutlet weak var videoTitle: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    func updateUI(partyRock: PartyRock) {
+        videoTitle.text = partyRock.videoTitle
+        
+        let url = URL(string: partyRock.imageURL)!
+        
+        DispatchQueue.global().async {
+            do {
+                let data = try Data(contentsOf: url)
+                DispatchQueue.global().sync {
+                    self.videoPreviewImage.image = UIImage(data: data)
+                }
+            } catch  {
+                //handle the error
+            }
+        }
 
-        // Configure the view for the selected state
     }
 
 }
